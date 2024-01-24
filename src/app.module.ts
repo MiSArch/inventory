845d6f62172c50cdf64d9e10ca2ff51b6945107b
k.langer@eloaded.eu <k.langer@eloaded.eu>
2024-01-25 00:52:44 +0100

    Removed db auth for now

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,10 +22,6 @@ import { MongooseModule } from '@nestjs/mongoose';
     InventoryModule,
     MongooseModule.forRoot(process.env.DATABASE_URI, {
       dbName: process.env.DATABASE_NAME,
-      auth: {
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASS,
-      },
     }),
   ],
 })
