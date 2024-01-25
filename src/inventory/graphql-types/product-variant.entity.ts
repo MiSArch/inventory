390c2cc8591a16e8e3a99bace2db6480916c451c
k.langer@eloaded.eu <k.langer@eloaded.eu>
2024-01-25 08:45:47 +0100

    Added logs, fixed issue with productVariant resolver

--- src/inventory/graphql-types/product-variant.entity.ts
+++ src/inventory/graphql-types/product-variant.entity.ts
@@ -1,11 +1,13 @@
 import { ObjectType, Field, Directive } from '@nestjs/graphql';
 import { UUID } from 'src/shared/scalars/CustomUuidScalar';
+import { ProductItemConnection } from './product-item-connection.dto';
 
 @ObjectType({ description: 'Foreign type ProductVariant' })
 @Directive('@key(fields: "id")')
 export class ProductVariant {
   @Field(() => UUID, {
     description: 'The uuid identifier of the product variant',
+    nullable: true
   })
   id: string;
 }
