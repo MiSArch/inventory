5515873f773f813b6f4e605166e98cce1f02a962
k.langer@eloaded.eu <k.langer@eloaded.eu>
2024-03-09 09:33:54 +0100

    Moved decorators below jsdocs, added docs for each orderDto field, regenerated graphql schema

--- src/events/dto/create-order.dto.ts
+++ src/events/dto/create-order.dto.ts
@@ -5,8 +5,11 @@ import { IsDate, IsEnum, IsNotEmpty, IsOptional, IsString, IsUUID, ValidateNeste
  * Enum representing the status of an order.
  */
 export enum OrderStatus {
+  // The order is pending and has not been placed yet.
   PENDING = 'PENDING',
+  // The order has been placed.
   PLACED = 'PLACED',
+  // The order has been rejected.
   REJECTED = 'REJECTED',
 }
 
@@ -14,7 +17,9 @@ export enum OrderStatus {
  * Enum representing the possible reasons for rejecting an order.
  */
 export enum RejectionReason {
+  // The order was rejected since data was invalid.
   INVALID_ORDER_DATA = 'INVALID_ORDER_DATA',
+  // The order was rejected since the product item reservation failed.
   INVENTORY_RESERVATION_FAILED = 'INVENTORY_RESERVATION_FAILED',
 }
 
@@ -22,9 +27,13 @@ export enum RejectionReason {
  * Enum representing the status of a shipment.
  */
 enum ShipmentStatus {
+  // The shipment is pending.
   PENDING = 'PENDING',
+  // The shipment is in progress.
   IN_PROGRESS = 'IN_PROGRESS',
+  // The shipment has been delivered.
   DELIVERED = 'DELIVERED',
+  // The shipment has failed.
   FAILED = 'FAILED',
 }
 
@@ -32,9 +41,11 @@ enum ShipmentStatus {
  * DTO representing a shipment.
  */
 export class ShipmentDTO {
+  // The status of the shipment.
   @IsEnum(ShipmentStatus)
   status: ShipmentStatus;
 
+  // The UUID of the used shipment method.
   @IsUUID()
   shipmentMethodId: string;
 
@@ -45,86 +56,102 @@ export class ShipmentDTO {
  * DTO representing an order.
  */
 export class OrderDTO {
-    @IsUUID()
-    id: string;
+  // The UUID of the order.
+  @IsUUID()
+  id: string;
 
-    @IsUUID()
-    userId: string;
+  // The UUID of the user who placed the order.
+  @IsUUID()
+  userId: string;
 
-    @IsDate()
-    createdAt: Date;
+  // The creation date of the order.
+  @IsDate()
+  createdAt: Date;
 
-    @IsEnum(OrderStatus)
-    orderStatus: OrderStatus;
+  // The current status of the order.
+  @IsEnum(OrderStatus)
+  orderStatus: OrderStatus;
 
-    @IsOptional()
-    @IsDate()
-    placedAt?: Date;
+  // The date the order was placed.
+  @IsOptional()
+  @IsDate()
+  placedAt?: Date;
 
-    @IsOptional()
-    @IsEnum(RejectionReason)
-    rejectionReason?: RejectionReason;
+  // The reason of rejection, if an order was rejected.
+  @IsOptional()
+  @IsEnum(RejectionReason)
+  rejectionReason?: RejectionReason;
 
-    @IsUUID()
-    shipmentAddressId: string;
+  // The UUID of the connected shipment address.
+  @IsUUID()
+  shipmentAddressId: string;
 
-    @IsUUID()
-    invoiceAddressId: string;
+  // The UUID of the connected invoice address.
+  @IsUUID()
+  invoiceAddressId: string;
 
-    @ValidateNested({ each: true })
-    @Type(() => OrderItemDTO)
-    @IsArray()
-    orderItems: OrderItemDTO[];
+  // The included order items.
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDTO)
+  @IsArray()
+  orderItems: OrderItemDTO[];
 }
 
 /**
  * DTO representing an order item.
  */
 export class OrderItemDTO {
-    @IsUUID()
-    id: string;
+  // The UUID of the order item.
+  @IsUUID()
+  id: string;
 
-    @IsDate()
-    createdAt: Date;
+  // The creation date of the order item.
+  @IsDate()
+  createdAt: Date;
 
-    @IsOptional()
-    @IsUUID()
-    productItemId?: string;
+  // The UUID of the items product variant version.
+  @IsUUID()
+  productVariantVersionId: string;
 
-    @IsUUID()
-    productVariantVersionId: string;
+  // The UUID of the items product variant.
+  @IsUUID()
+  productVariantId: string;
 
-    @IsUUID()
-    productVariantId: string;
+  // The UUID of the items tax rate version.
+  @IsUUID()
+  taxRateVersionId: string;
 
-    @IsUUID()
-    taxRateVersionId: string;
+  // The UUID of the original shopping cart item.
+  @IsUUID()
+  shoppingCartItemId: string;
 
-    @IsUUID()
-    shoppingCartItemId: string;
+  // The ordered count of the product Variant Version.
+  @IsInt()
+  @Min(1)
+  count: number;
 
-    @IsInt()
-    @Min(1)
-    count: number;
+  // The amount to be compensated for this order item in case of a return.
+  @IsInt()
+  @Min(0)
+  compensatableAmount: number;
 
-    @IsInt()
-    @Min(0)
-    compensatableAmount: number;
+  // The shipment for the order item.
+  @ValidateNested()
+  @Type(() => ShipmentDTO)
+  shipment: ShipmentDTO;
 
-    @ValidateNested()
-    @Type(() => ShipmentDTO)
-    shipment: ShipmentDTO;
-
-    @IsArray()
-    @IsUUID("4", { each: true })
-    discountIds: string[];
+  // The UUIDs of the discounts applied to the order item.
+  @IsArray()
+  @IsUUID("4", { each: true })
+  discountIds: string[];
 }
 
 /**
  * DTO representing a create order event payload.
  */
 export class CreateOrderDto {
-    @ValidateNested()
-    @Type(() => OrderDTO)
-    order: OrderDTO;
+  // The order data.
+  @ValidateNested()
+  @Type(() => OrderDTO)
+  order: OrderDTO;
 }
