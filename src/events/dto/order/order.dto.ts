3bd0c1db9cc2cc778373a81d408e7e58536b9443
k.langer@eloaded.eu <k.langer@eloaded.eu>
2024-03-20 19:05:31 +0100

    Updated order dto

--- src/events/dto/order/order.dto.ts
+++ src/events/dto/order/order.dto.ts
@@ -1,5 +1,13 @@
 import { Type } from 'class-transformer';
-import { IsDate, IsEnum, IsOptional, IsUUID, ValidateNested, IsArray } from 'class-validator';
+import {
+  IsDate,
+  IsEnum,
+  IsOptional,
+  IsUUID,
+  ValidateNested,
+  IsArray,
+  IsInt,
+} from 'class-validator';
 import { OrderStatus } from './order-status';
 import { RejectionReason } from './order-rejection-reason';
 import { OrderItemDTO } from './order-item.dto';
@@ -11,11 +19,13 @@ import { OrderItemDTO } from './order-item.dto';
  * @property userId UUID of user connected with Order.
  * @property createdAt Timestamp when Order was created.
  * @property orderStatus The status of the Order.
+ * @property compensatableOrderAmount Total cost of all order items after shipping and discounts.
  * @property placedAt Timestamp of Order placement. Not present until Order is placed.
  * @property rejectionReason The rejection reason if status of the Order is REJECTED.
  * @property orderItems List of OrderItems associated with the Order.
  * @property shipmentAddressId UUID of shipment address associated with the Order.
  * @property invoiceAddressId UUID of invoice address associated with the Order.
+ * @property paymentInformationId UUID of payment information associated with the Order.
  */
 export class OrderDTO {
   @IsUUID()
@@ -26,6 +36,8 @@ export class OrderDTO {
   createdAt: Date;
   @IsEnum(OrderStatus)
   orderStatus: OrderStatus;
+  @IsInt()
+  compensatableOrderAmount: number;
   @IsOptional()
   @IsDate()
   placedAt?: Date;
@@ -40,4 +52,6 @@ export class OrderDTO {
   shipmentAddressId: string;
   @IsUUID()
   invoiceAddressId: string;
+  @IsUUID()
+  paymentInformationId: string;
 }
